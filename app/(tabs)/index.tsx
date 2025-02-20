9a829b14171aea5829e6b28d3a3b328921be375f
Caleb Westra <calebwestra2002@gmail.com>
2025-02-20 11:51:29 -0600

    formatted buttons/text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,23 +8,53 @@ export default function stopWatch() {
     if(!run) return
     const id = setInterval(()=>setTime(time=>time+1),10)
     return () => clearInterval(id)
-
   }, [run])
 
-
-
   return (
-    <View>
-      <Text>{time/100}</Text>
-      <View>
-        <Button onPress={()=>setRun(!run)} title={run ? 'Pause' : 'Start'}></Button>
+    <View style={styles.page}>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>{(time/100).toFixed(2)}</Text>
       </View>
-      <View>
-        <Button onPress={()=>{setRun(false); setTime(0)}} title='reset'></Button>
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttons}>
+          <Button onPress={()=>setRun(!run)} title={run ? 'Pause' : 'Start'}></Button>
+        </View>
+        <View style={styles.buttons}>
+          <Button onPress={()=>{setRun(false); setTime(0)}} title='reset'></Button>
+        </View>
+        <View style={styles.buttons}>
+          <Button title='Lap'></Button>
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  page: {
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textContainer: {
+    width: 300,
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderRadius: 150
+  },
+  text: {
+    alignSelf: 'center',
+    fontFamily: 'monospace',
+    fontSize: 40
+  },
+  buttonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    margin: 10
+  },
+  buttons : {
+    margin: 5
+  }
 });
