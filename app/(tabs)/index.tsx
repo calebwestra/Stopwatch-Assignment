939c3abb42b252f66776a3b4af9ce7e7063c6f24
Caleb Westra <calebwestra2002@gmail.com>
2025-02-21 15:06:15 -0600

    time formatting (01 instead of 1) and added minutes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,37 @@
 import { StyleSheet, View, Button, Text } from 'react-native';
 import { useState, useEffect} from 'react';
 
-export default function stopWatch() {
-  const [time, setTime] = useState(0)
+export default function StopWatch() {
+  const [milisecs, setSecs] = useState(5500)
+  const [mins, setMins] = useState(0)
   const [run, setRun] = useState(false)
-  useEffect(()=>{
-    if(!run) return
-    const id = setInterval(()=>setTime(time=>time+1),10)
-    return () => clearInterval(id)
-  }, [run])
+  useEffect(() => {
+    if (!run) return;
+    const id = setInterval(()=>{
+      setSecs(milisecs=>{
+        if (milisecs >= 6000) {
+          setMins(mins=>mins+1)
+          return 0
+        } 
+          return milisecs + 1
+        })
+      }, 10)
+    return () => clearInterval(id);
+  }, [run]);
 
   return (
     <View style={styles.page}>
       <View style={styles.textContainer}>
-        <Text style={styles.text}>{(time/100).toFixed(2)}</Text>
+        <Text style={styles.text}>{String(mins).padStart(2, '0') + ':' + 
+              String(Math.floor(milisecs/100)).padStart(2,'0') + '.' +
+              String(milisecs % 100).padStart(2, '0')}</Text>
       </View>
       <View style={styles.buttonContainer}>
         <View style={styles.buttons}>
           <Button onPress={()=>setRun(!run)} title={run ? 'Pause' : 'Start'}></Button>
         </View>
         <View style={styles.buttons}>
-          <Button onPress={()=>{setRun(false); setTime(0)}} title='reset'></Button>
+          <Button onPress={()=>{setRun(false); setSecs(0); setMins(0)}} title='reset'></Button>
         </View>
         <View style={styles.buttons}>
           <Button title='Lap'></Button>
